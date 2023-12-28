21bee010a7768fe5665c077aa5565b2780b3c260
Yuresh Majhi <yureshmee@gmail.com>
2023-12-28 11:28:30 +0545

    form css minor updates

--- src/components/Admin/Product/AddProduct.jsx
+++ src/components/Admin/Product/AddProduct.jsx
@@ -23,71 +23,105 @@ const AddProduct = () => {
         onSubmit={handleSubmit}
         className="flex flex-col items-start gap-6 p-4 bg-[#08424038] max-w-lg ml-6 rounded-lg border-2 border-[#084240]"
       >
-        <h2 className="text-4xl text-[#084240] mb-4">Add New Product</h2>
+        <h2 className="text-4xl text-[#084240] mb-4 underline">
+          Add New Product
+        </h2>
 
         {/*  result */}
         {/* {showError()}
         {showSuccess()} */}
 
-        <label htmlFor="title">Product Name</label>
-        <input
-          type="text"
-          placeholder="Enter New Product"
-          id="title"
-          onChange={(e) => setProduct(e.target.value)}
-          className="p-2 rounded w-full"
-        />
+        <div className="w-full">
+          <label className="text-[#084240] font-bold text-lg" htmlFor="title">
+            Product Name
+          </label>
+          <input
+            type="text"
+            placeholder="Enter New Product"
+            id="title"
+            onChange={(e) => setProduct(e.target.value)}
+            className="p-2 rounded w-full"
+          />
+        </div>
 
-        <label htmlFor="price">Price</label>
-        <input
-          type="number"
-          placeholder="Enter Price"
-          id="price"
-          onChange={(e) => setProduct(e.target.value)}
-          className="p-2 rounded w-full"
-        />
+        <div className="w-full">
+          <label className="text-[#084240] font-bold text-lg" htmlFor="price">
+            Price
+          </label>
+          <input
+            type="number"
+            placeholder="Enter Price"
+            id="price"
+            onChange={(e) => setProduct(e.target.value)}
+            className="p-2 rounded w-full"
+          />
+        </div>
 
-        <label htmlFor="description">Description</label>
-        <textarea
-          placeholder="Describe your product"
-          id="description"
-          rows={4}
-          onChange={(e) => setProduct(e.target.value)}
-          className="p-2 rounded w-full resize-none"
-        ></textarea>
+        <div className="w-full">
+          <label
+            className="text-[#084240] font-bold text-lg"
+            htmlFor="description"
+          >
+            Description
+          </label>
+          <textarea
+            placeholder="Describe your product"
+            id="description"
+            rows={4}
+            onChange={(e) => setProduct(e.target.value)}
+            className="p-2 rounded w-full resize-none"
+          ></textarea>
+        </div>
 
-        <label htmlFor="count_in_stock">Count in Stock</label>
-        <input
-          type="number"
-          placeholder="Stock"
-          id="count_in_stock"
-          onChange={(e) => setProduct(e.target.value)}
-          className="p-2 rounded w-full"
-        />
+        <div className="w-full">
+          <label
+            className="text-[#084240] font-bold text-lg"
+            htmlFor="count_in_stock"
+          >
+            Count in Stock
+          </label>
+          <input
+            type="number"
+            placeholder="Stock"
+            id="count_in_stock"
+            onChange={(e) => setProduct(e.target.value)}
+            className="p-2 rounded w-full"
+          />
+        </div>
 
-        <label htmlFor="image">Image</label>
-        <input
-          type="file"
-          placeholder="Image"
-          id="image"
-          onChange={(e) => setProduct(e.target.value)}
-          className="p-2 rounded w-full"
-        />
+        <div className="w-full">
+          <label className="text-[#084240] font-bold text-lg" htmlFor="image">
+            Image
+          </label>
+          <input
+            type="file"
+            placeholder="Image"
+            id="image"
+            onChange={(e) => setProduct(e.target.value)}
+            className="p-2 rounded w-full"
+          />
+        </div>
 
-        <label htmlFor="category">Select Category</label>
-        <select id="category" defaultValue="" className="p-2 rounded w-full">
-          <option value="" disabled>
-            Choose a Category
-          </option>
-
-          {/* Category list from api */}
-          {categories &&
-            categories.map((category, i) => (
-              <option key={i} value={category.title}>
-                {category.title}
-              </option>
-            ))}
-        </select>
+        <div className="w-full">
+          <label
+            className="text-[#084240] font-bold text-lg"
+            htmlFor="category"
+          >
+            Select Category
+          </label>
+          <select id="category" defaultValue="" className="p-2 rounded w-full">
+            <option value="" disabled>
+              Choose a Category
+            </option>
+            {/* Category list from api */}
+            {categories &&
+              categories.map((category, i) => (
+                <option key={i} value={category.title}>
+                  {category.title}
+                </option>
+              ))}
+          </select>
+        </div>
         <button className="bg-[#084240] text-white p-3 rounded w-[105px] border-2 border-[#08424000] mx-auto">
           Add
         </button>
