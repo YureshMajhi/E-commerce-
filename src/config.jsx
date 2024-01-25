fca2115d4a5759974228d3778717107936fc1b05
Yuresh Majhi <yureshmee@gmail.com>
2024-01-25 13:10:53 +0545

    minimal url update

--- src/config.jsx
+++ src/config.jsx
@@ -1,2 +1,2 @@
 // export const API = import.meta.env.VITE_REACT_APP_BACKEND_URL;
-export const API = "https://yuresh-store-backend.onrender.com/";
+export const API = "https://yuresh-store-backend.onrender.com";
