f9f9bc09691f479963f5092be86fa134e5f1c79d
Yuresh Majhi <yureshmee@gmail.com>
2024-01-25 13:08:51 +0545

    backend url

--- src/config.jsx
+++ src/config.jsx
@@ -1,1 +1,2 @@
-export const API = import.meta.env.VITE_REACT_APP_BACKEND_URL;
+// export const API = import.meta.env.VITE_REACT_APP_BACKEND_URL;
+export const API = "https://yuresh-store-backend.onrender.com/";
